cb4bd1fdaf8c5ce69546f662e84f91cacb727f7c
Lein3 <semen.shatilo@mail.ru>
2021-12-07 19:39:57 +0700

    Добавьте файлы проекта.

---
+++ ClientTCP/ConnectionTCP.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTCP
+{
+    public static class ConnectionTCP
+    {
+        public static string ip { get; set; } = "127.0.0.1";
+        public static int port { get; set; } = 8080;
+        public static IPEndPoint tcpEndPoint { get; set; } = new IPEndPoint(IPAddress.Parse(ip), port);
+        public static Socket tcpSocket { get; set; } = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public static void Send()
+        {
+            Console.WriteLine("Введите сообщение");
+            var message = Console.ReadLine();
+            var data = Encoding.UTF8.GetBytes(message);
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
+
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+            do
+            {
+                size = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            } while (tcpSocket.Available > 0);
+
+            Console.WriteLine(answer);
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+        }
+    }
+}
---
+++ ConnectionTCP.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTCP
+{
+    public static class ConnectionTCP
+    {
+        public static string ip { get; set; } = "127.0.0.1";
+        public static int port { get; set; } = 8080;
+        public static IPEndPoint tcpEndPoint { get; set; } = new IPEndPoint(IPAddress.Parse(ip), port);
+        public static Socket tcpSocket { get; set; } = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public static void Start()
+        {
+            tcpSocket.Bind(tcpEndPoint);
+            tcpSocket.Listen(5);
+            while(true)
+            {
+                var listener = tcpSocket.Accept();
+                var buffer = new byte[256];
+                var size = 0;
+                var data = new StringBuilder();
+                do
+                {
+                    size = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                } while (listener.Available > 0);
+
+                Console.WriteLine(data);
+                listener.Send(Encoding.UTF8.GetBytes("Успех"));
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+        }
+    }
+}
